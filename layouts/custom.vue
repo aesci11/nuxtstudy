c3e1d296e45084b99ee136035710b49fd733d70e
aesci11 <137674871+aesci11@users.noreply.github.com>
2023-09-01 00:49:34 +0900

    Add files via upload

---
+++ layouts/custom.vue
@@ -0,0 +1,14 @@
+<template>
+  <main class="h-screen flex flex-col">
+    <div>
+      <Sidebar />
+    </div>
+    <div>
+      <slot />
+    </div>
+  </main>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
